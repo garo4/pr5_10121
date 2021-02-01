3d19ada1227a50e551bf5860c729456350777da0
Слепцов Никита ИСИП19 <SleptsovISIP19@iMac-Mac007.local>
2021-02-01 15:43:47 +0900

    First commit

---
+++ app/src/main/java/com/example/multiscreen/Uruguay.java
@@ -0,0 +1,14 @@
+package com.example.multiscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Uruguay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_uruguay);
+    }
+}
